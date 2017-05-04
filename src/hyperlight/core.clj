f8920c372c1e7836d9fec70fed31450780406c3d
Max Countryman <maxc@me.com>
2017-05-04 15:57:55 -0700

    provide helpful handler middleware

--- src/hyperlight/core.clj
+++ src/hyperlight/core.clj
@@ -7,7 +7,7 @@
     {:connections-per-host 256
      :total-connections 2048
      :connection-options
-     {:raw-stream? true :keep-alive? true :insecure? true}}))
+     {:keep-alive? true :insecure? true}}))
 
 (defn format-set-cookies
   "Formats set-cookie headers."
@@ -16,6 +16,17 @@
     (assoc-in rsp [:headers "set-cookie"] (into [] set-cookie))
     rsp))
 
+(defn- get-x-forwarded-for
+  [req]
+  (if-let [forwarded-for (get-in req [:headers "x-forwarded-for"])]
+    (str forwarded-for "," (:remote-addr req))
+    (:remote-addr req)))
+
+(defn format-x-forwarded
+  "Formats x-forwarded headers."
+  [req]
+  (assoc-in req [:headers "x-forwarded-for"] (get-x-forwarded-for req)))
+
 (defn wrap-format-set-cookies
   "Wraps formatting of set-cookie headers."
   [handler]
@@ -23,36 +34,55 @@
     (d/let-flow [rsp (handler req)]
       (format-set-cookies rsp))))
 
+(defn wrap-x-forwarded
+  "Wraps formatting of x-forwarded headers."
+  [handler]
+  (fn [req]
+    (handler (format-x-forwarded req))))
+
 (defn proxy-req
   "Proxies a request."
-  [{headers :headers :as req} req-options]
+  [{:keys [body headers query-string request-method uri]
+    :as req}
+   {:keys [port scheme server-name url]
+    :as req-options}]
   (let [default-req-options
         {:pool default-pool
          :throw-exceptions? false
-         :follow-redirects? false}]
-    (d/chain
+         :follow-redirects? false}
+        uri-options
+        (if url
+          {:url (str url uri)}
+          {:port port :scheme scheme :server-name server-name :uri uri})]
+    (d/chain'
       (http/request
         (merge
-          req
+          {:body body
+           :headers headers
+           :query-string query-string
+           :request-method request-method}
           default-req-options
-          req-options)))))
+          req-options
+          uri-options)))))
 
 (defn create-proxy-handler
   "Creates a proxy handler."
-  [scheme port server-name]
-  (-> (fn [req]
-        (proxy-req
-          req
-          {:scheme scheme
-           :port port
-           :server-name server-name}))
-      wrap-format-set-cookies))
+  [{:keys [format-set-cookies? format-x-forwarded?]
+    :or {format-set-cookies? true format-x-forwarded? true}
+    :as req-options}]
+  (as-> #(proxy-req % req-options) handler
+    (if format-set-cookies?
+      (wrap-format-set-cookies handler)
+      handler)
+    (if format-x-forwarded?
+      (wrap-x-forwarded handler)
+      handler)))
 
 (defn start-proxy-server
   "Starts a proxy server."
   ([handler]
    (start-proxy-server
      handler
-     {:port 3000 :raw-stream? true :executor :none}))
+     {:port 3000 :executor :none}))
   ([handler options]
    (http/start-server handler options)))
