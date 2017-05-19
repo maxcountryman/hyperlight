8c623a2dda63fdeec4772629dcec53a510160d47
Max Countryman <maxc@me.com>
2017-05-19 15:50:04 -0700

    bump aleph version to ensure content-length is used when provided

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject hyperlight "0.1.1"
   :description "A performance-focused HTTP reverse proxy"
   :license {:name "Eclipse Public License"}
-  :dependencies [[aleph "0.4.4-alpha2"]
+  :dependencies [[aleph "0.4.4-alpha4"]
                  [org.clojure/clojure "1.9.0-alpha14"]]
   :jvm-opts ^:replace ["-server"
                        "-XX:+UseConcMarkSweepGC"
