eb0b7d7917cbd36ffe88effb3bf5370c89c16f5e
Max Countryman <maxc@me.com>
2017-05-21 18:23:13 -0700

    simplify middleware tests

--- test/hyperlight/middleware_test.clj
+++ test/hyperlight/middleware_test.clj
@@ -16,30 +16,6 @@
   (let [base-request
         {:scheme :http
          :remote-addr "0.0.0.0"
-         :headers {"x-forwarded-for" "1.2.3.4"}}]
-    (is (= (middleware/format-x-forwarded base-request)
-           (merge base-request {:headers {"x-forwarded-for" "1.2.3.4, 0.0.0.0"
-                                          "x-forwarded-port" "80"
-                                          "x-forwarded-proto" "http"}}))))
-  (let [base-request
-        {:scheme :http
-         :remote-addr "0.0.0.0"
-         :headers {"x-forwarded-port" "443"}}]
-    (is (= (middleware/format-x-forwarded base-request)
-           (merge base-request {:headers {"x-forwarded-for" "0.0.0.0"
-                                          "x-forwarded-port" "443, 80"
-                                          "x-forwarded-proto" "http"}}))))
-  (let [base-request
-        {:scheme :http
-         :remote-addr "0.0.0.0"
-         :headers {"x-forwarded-proto" "https"}}]
-    (is (= (middleware/format-x-forwarded base-request)
-           (merge base-request {:headers {"x-forwarded-for" "0.0.0.0"
-                                          "x-forwarded-port" "80"
-                                          "x-forwarded-proto" "https, http"}}))))
-  (let [base-request
-        {:scheme :http
-         :remote-addr "0.0.0.0"
          :headers {"x-forwarded-for" "1.2.3.4"
                    "x-forwarded-port" "443"
                    "x-forwarded-proto" "https"}}]
