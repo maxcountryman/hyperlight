8b27db2ca0d90bdcd361159547103efddcd592f8
Max Countryman <maxc@me.com>
2017-06-23 15:37:41 -0700

    mark 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject hyperlight "0.3.0"
+(defproject hyperlight "0.3.1"
   :description "A performance-focused HTTP reverse proxy"
   :license {:name "Eclipse Public License"}
   :dependencies [[aleph "0.4.4-alpha4"]
